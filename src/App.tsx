7b0740a086b868c9f856320256ed47152a7be798
jankov <jankov@tutanota.com>
2020-03-19 17:14:00 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ButtonAppBar from './shared/components/nav-bar';
+
+function App() {
+  return (
+    <div className="App">
+	<ButtonAppBar></ButtonAppBar>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/shared/components/nav-bar.tsx
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
+
+export default function ButtonAppBar() {
+  
+  const classes = useStyles();
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const onClickLogin = () => {
+    console.log("Clicked")
+    setIsLoggedIn(true)
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={onClickLogin}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          {
+            !isLoggedIn ? (<Button color="inherit">Login</Button>) : null
+          }
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
